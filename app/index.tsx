287faae043dc3aeed6b1e9a2ca1fd6c42793d9fa
Shahriar Azam Khan <shahriarazamkhan@gmail.com>
2025-07-29 22:59:08 +0600

    Initital

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit apps/index.tsx to edit this screen11.</Text>
+    </View>
+  );
+}
